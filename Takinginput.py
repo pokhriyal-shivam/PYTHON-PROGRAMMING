48890bb6b90419e23f59593f5452abe923b45d15
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-09-27 19:34:24 +0530

    Takinginput.py

---
+++ Takinginput.py
@@ -0,0 +1,19 @@
+fruits=[]
+
+
+f1=input("enter 1st fruit  \n")
+fruits.append(f1)
+
+f2=input("enter 1st fruit  \n")
+fruits.append(f2)
+
+f3=input("enter 1st fruit  \n")
+fruits.append(f3)
+
+f4=input("enter 1st fruit  \n")
+fruits.append(f4)
+
+f5=input("enter 1st fruit  \n")
+fruits.append(f5)
+
+print(fruits)
