4081533bf98194a9c01b1afe7ca7325c8d2edb6a
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-09-27 19:54:35 +0530

    Create Sum_of_List.py
     program to sum numbers in a list

---
+++ Sum_of_List.py
@@ -0,0 +1,21 @@
+# Program to sum the input numbers in the list
+
+Numbers=[]
+
+num1=int(input("enter 1st number"))
+Numbers.append(num1)
+
+num2=int(input("enter 1st number"))
+Numbers.append(num2)
+
+num3=int(input("enter 1st number"))
+Numbers.append(num3)
+
+num4=int(input("enter 1st number"))
+Numbers.append(num4)
+
+print(Numbers)
+
+Total_sum=sum(Numbers)
+
+print("Total sum of the numbers in the list are:",Total_sum)
