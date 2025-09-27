d90e6552978dce45d25fe725536f24b642154ec8
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-09-27 19:36:13 +0530

    Create  marks_sorting.py
    add marks sorting program

---
+++ marks_sorting.py
@@ -0,0 +1,28 @@
+#to take the marks input and print them in sorted manner
+
+MARKS=[]
+
+m1=input("enter MARKS 1") 
+MARKS.append(m1)
+
+m2=input("enter MARKS 1")
+MARKS.append(m2)
+
+m3=input("enter MARKS 1") 
+MARKS.append(m3)
+
+m4=input("enter MARKS 1")
+MARKS.append(m4)
+
+
+# for ascending
+MARKS.sort()
+
+
+
+#for desecnding 
+
+# MARKS.sort(reverse=True)
+
+
+print (MARKS)
