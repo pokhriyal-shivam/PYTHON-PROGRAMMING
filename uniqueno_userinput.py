d3290ee8d435d1b54199ff600779c71f6e611619
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-02 01:10:32 +0530

    Create uniqueno_userinput.py
    program to take 5 numbers from user and display all unique numbers(once)

---
+++ uniqueno_userinput.py
@@ -0,0 +1,38 @@
+# program to take 5 numbers from user and display all unique numbers(once)
+
+s = set()
+
+n= input("enter first no")
+s.add(int(n))
+
+n= input("enter second no")
+s.add(int(n))
+
+n= input("enter third no")
+s.add(int(n))
+
+n= input("enter fourth no")
+s.add(int(n))
+
+n= input("enter fifth no")
+s.add(int(n))
+
+
+print(s)
+
+
+
+
+
+output:
+
+enter first no2
+enter second no3
+enter third no2
+enter fourth no2
+enter fifth no5
+{2, 3, 5}
+
+
+
+
