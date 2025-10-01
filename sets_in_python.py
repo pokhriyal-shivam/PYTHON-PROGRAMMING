2365481884628e4d330f2137c2caf584f17aeb30
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-01 23:09:27 +0530

    Create sets_in_python.py
    diff methods executing in sets using python

---
+++ sets_in_python.py
@@ -0,0 +1,12 @@
+s = {1,2,3,445,44,"shivam",57,88,99,58,96,63}
+
+# s.add(88)    # to add
+
+# s.remove(88)   #to remove
+
+s.pop() #to pop random element
+
+# s.clear()   #clear all elements
+
+
+print(s,type(s))
