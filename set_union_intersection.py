45da1f972354b47510e43ee6f2b99d6322969023
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-02 00:42:27 +0530

    Create set_union_intersection.py

---
+++ set_union_intersection.py
@@ -0,0 +1,18 @@
+
+SOURCE CODE:
+
+set1 = {2,4,5,6,7}
+
+set2 = {4,5,2,99,45}
+
+print("this is union",set1.union(set2))
+
+print("this is intersection",set1.intersection(set2))
+
+
+
+
+OUTPUT:
+
+This is Union: {2, 4, 5, 6, 7, 99, 45}
+This is Intersection: {2, 4, 5}
