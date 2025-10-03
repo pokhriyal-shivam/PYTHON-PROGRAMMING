ad88c28c765544536faa7d8c5fe8f809a296f6bb
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-03 23:07:12 +0530

    Create conditional_statement.py
    program to check which age grp are eligible to vote.....

---
+++ conditional_statement.py
@@ -0,0 +1,14 @@
+Age = int(input("enter your age for the eligibility of voting"))
+
+if(Age>=18):
+    print("congratulations! you are eligible for vote")
+
+elif(Age<=0):
+    print("Ahh you are entering a invalid number! please enter valid age")
+
+
+else:
+    print("no! you are not eligible\n")
+
+
+print("end of the program !!! ")
