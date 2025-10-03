45b0f433895ae67c7758b436a8bb09992d33bb45
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-03 23:18:51 +0530

    Create loops_operations.py
    we'll see how loops can be used in different places using python

---
+++ loops_operations.py
@@ -0,0 +1,33 @@
+
+# loop use to print string
+
+string="MASTER SHIVAM POKHRIYAL"
+for  i in string:
+    print(i)
+
+
+
+#using loop with condtional statement
+
+L=[1,2,3,4,8]
+for i in L:
+    print(i)
+else:
+    print(" end ")
+
+  
+
+
+
+# # to break loop at specific no 
+for i in range(100):
+    if(i==97):
+        break
+    print(i)
+
+
+# to skip trhe iteration at particular no 
+for i in range(100):
+    if(i==97):
+        continue # skip the iteration 
+    print(i)
