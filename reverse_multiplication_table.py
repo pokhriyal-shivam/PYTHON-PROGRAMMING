e0bfe2b56d1e33cfa6d81234d272f22b59490b1d
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-10-16 00:04:45 +0530

    Create reverse_multiplication_table.py
    To print any user given number multiplication table in reversed order

---
+++ reverse_multiplication_table.py
@@ -0,0 +1,26 @@
+#to print a tble in reverse order.....
+
+# input the number
+n = int(input("enter a number"))
+
+# loop for no range
+for i in range(1,11):
+
+
+# printing the final reversed table
+    print(f"{n} * {11-i}  ={n*(11-i)}" )
+
+
+
+output :-
+enter a number11
+11 * 10  =110
+11 * 9  =99
+11 * 8  =88
+11 * 7  =77
+11 * 6  =66
+11 * 5  =55
+11 * 4  =44
+11 * 3  =33
+11 * 2  =22
+11 * 1  =11
