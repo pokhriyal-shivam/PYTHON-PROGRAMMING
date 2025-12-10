493c7716f869d6222898e6574c6f3814a5a72ac4
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-12-10 13:40:49 +0530

    Add nested loops to print pairs of numbers

---
+++ nested loops.py
@@ -0,0 +1,13 @@
+# Program: Print all pairs (i,j) where i,j from 1 to 3
+
+def print_pairs():
+    for i in range(1, 4):
+        for j in range(1, 4):
+            print(f"Pair: ({i}, {j})")
+
+def main():
+    print("All pairs (i,j) from 1 to 3:")
+    print_pairs()
+
+
+    main()
