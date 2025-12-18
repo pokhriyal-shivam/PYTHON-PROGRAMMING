3c47aa755def5a63cf473c4b938d505318120d1e
Shivam Pokhriyal <pokhriyalshivam87@gmail.com>
2025-12-18 21:41:06 +0530

    Implement counting class with increase method

---
+++ count function.py
@@ -0,0 +1,20 @@
+class counting:
+    def __init__(self):
+        self.count=0
+    
+    def increase(self):
+        self.count+=1
+
+obj = counting()
+
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+obj.increase()
+print(obj.count)
